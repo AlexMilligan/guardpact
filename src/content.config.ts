9bd971387bb34e7832cf76d99173897363e4c77b
alexander.ryan.milligan <alexander.ryan.milligan@gmail.com>
2025-11-24 21:50:42 +0000

    add blog schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,18 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+const blogSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  publishDate: z.date().optional(),
+  layout: z.string().optional(),
+});
+
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  blog: defineCollection({
+    type: 'content',
+    schema: blogSchema,
+  }),
 };
