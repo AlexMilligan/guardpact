20c8cf7412f9285f63476bfce80479477ffb8aea
alexander.ryan.milligan <alexander.ryan.milligan@gmail.com>
2025-11-28 09:36:17 +0000

    chore: Remove web-app.astro from tracking

--- web-app/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZpwVrI from "../../assets/Guard Pact Logo.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/Guard Pact Logo.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZpwVrI]]);
-		
